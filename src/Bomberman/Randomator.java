5429ebbf81a06baa005e21611e876bd35adaa408
Sébastien Maes <nautilussebastien@gmail.com>
2018-12-11 22:51:43 +0100

    remove comment

--- src/Bomberman/Randomator.java
+++ src/Bomberman/Randomator.java
@@ -4,8 +4,7 @@ import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Randomator {
-
-    //TODO return an int between inf and supp-1
+    
     public static int rand(int min, int max)
     {
         return ThreadLocalRandom.current().nextInt(min, max + 1); // add 1 to include the max. nextInt is normally exclusive
