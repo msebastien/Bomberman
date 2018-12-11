00097636e18e0ec3d835f385e82f367d7ee87d21
Sébastien Maes <nautilussebastien@gmail.com>
2018-12-11 22:23:06 +0100

    Add missing enum

--- src/Bomberman/Direction.java
+++ src/Bomberman/Direction.java
@@ -3,7 +3,7 @@ package Bomberman;
 import java.awt.Point;
 
 public enum Direction {
-    NORTH(0,-1), SOUTH(0,1), EAST(1,0), WEST(-1, 0);
+    NORTH(0,-1), SOUTH(0,1), EAST(1,0), WEST(-1, 0), IDLE(0,0);
 
     protected Point direction;
     Direction(int x, int y) {
