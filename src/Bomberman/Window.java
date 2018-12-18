22f8323e12b7b9da9062de1321b4a94745e48357
EmileLevast <emilelevast@outlook.fr>
2018-12-18 13:59:36 +0100

    Emile reglage Fenetre dimension

--- src/Bomberman/Window.java
+++ src/Bomberman/Window.java
@@ -6,11 +6,19 @@ import java.awt.*;
 
 public class Window extends JFrame  {
 
+    //pour avoir la bonne taille de JPanel
+    private final int heightBorder=40;
+    private final int widthBorder=6;
+
+
     public Window(JPanel pan) throws HeadlessException {
+
+
         this.setTitle("Bomberman");
-        this.setSize(1000, 1000);
+        this.setSize(1000+widthBorder, 1000+heightBorder);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         this.setVisible(true);
         this.setResizable(false);
 
