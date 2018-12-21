17edc9589100c32b6677633003344e4c4f144e90
Sébastien Maes <nautilussebastien@gmail.com>
2018-12-21 17:22:35 +0100

    Entity class : code clean up

--- src/Bomberman/EntityManager/Entity.java
+++ src/Bomberman/EntityManager/Entity.java
@@ -12,9 +12,6 @@ public abstract class Entity {
     protected Point posInPixelMap;
     Container container; // contains images for animation
 
-    //check if we can delete the entity from the entity list
-    //protected boolean isAlive;
-
     public Entity(Point posInArrayMap)
     {
         init(posInArrayMap);
@@ -22,23 +19,15 @@ public abstract class Entity {
 
     //just used to call super from moving entity
     public Entity() {
-        container = new Container(); // Create container with the idle "animation" for the player
+        container = new Container();
     }
 
     public void init(Point posInArrayMap)
     {
-        //isAlive=true;
+        
         this.posInArrayMap=new Point(posInArrayMap);
         this.posInPixelMap=new Point(posInArrayMap.x* Map.WIDTH_TILE,posInArrayMap.y*Map.HEIGHT_TILE);
     }
-
-    /*public boolean isAlive() {
-        return isAlive;
-    }
-
-    public void destroy() {
-        isAlive = false;
-    }*/
 
     public abstract void action();
 
